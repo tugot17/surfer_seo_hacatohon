b42cb6a580d789a056309e99dac563689db6303d
ProtossLuigi <protoss0@wp.pl>
2021-10-23 13:50:09 +0200

    model done

--- model.py
+++ model.py
@@ -51,24 +51,43 @@ def herbert_forward(data, batch_size=256):
 
 import xgboost as xgb
 
-model = xgb.XGBRegressor()
-encoder = OneHotEncoder(handle_unknown='ignore')
+model = xgb.XGBRegressor(tree_method='gpu_hist') if torch.cuda.is_available() else xgb.XGBRegressor()
 
-def train(batch_size=256, validate=False):
-    columns = ['language', 'category', 'query', 'position', 'audit_latitude', 'audit_longitude']
+def train(validate=False):
+    columns = ['language', 'category', 'query', 'position', 'audit_latitude', 'audit_longitude', 'place_latitude', 'place_longitude']
     places = pd.read_csv('places.csv.gz')[columns].dropna()
     places = places[places['language'] == 'pl'][places['category'].map(places['category'].value_counts()) > THRESHOLD]
     X = pd.DataFrame(herbert_forward(list(places['query'])).numpy())
-    encoder.fit(places['category'])
-    X[encoder.categories_] = encoder.transform(places['category']).toarray()
+    # encoder.fit(places['category'].values.reshape(-1,1))
+    # cat_df = pd.DataFrame(encoder.transform(places['category'].values.reshape(-1,1)), columns=encoder.categories_[0].tolist())
+    # X = pd.concat([X, cat_df], axis=1)
+    # X[encoder.categories_[0].tolist()] = cat_df
+    places = places.reset_index()
     X['audit_latitude'] = places['audit_latitude']
     X['audit_longitude'] = places['audit_longitude']
+    X['place_latitude'] = places['place_latitude']
+    X['place_longitude'] = places['place_longitude']
     # X.fillna(len(categories.id_cat))
     y = places['position']
-    # print(y.isna())
     model.fit(X, y)
     if validate:
         y_pred = model.predict(X)
         print(mean_squared_error(y, y_pred))
 
 
+def make_query(query_msg, audit_latitude, audit_longitude, place_latitude, place_longitude):
+    query = pd.DataFrame(herbert_forward([query_msg]).numpy())
+    query['audit_latitude'] = audit_latitude
+    query['audit_longitude'] = audit_longitude
+    query['place_latitude'] = place_latitude
+    query['place_longitude'] = place_longitude
+    return model.predict(query)
+
+def save(filename):
+    model.save_model(filename)
+
+def init(filename=''):
+    if filename:
+        model.load_model(filename)
+    else:
+        train()
